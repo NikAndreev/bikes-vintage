a3cc532b454205556161930ee2c74dff32d847ac
NikAndreev <57640150+NikAndreev@users.noreply.github.com>
2020-02-25 22:59:44 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+let dots = document.querySelector('.slider__dots').children;
+	slidesArea = document.querySelector('.slider__slides');
+	slides = ['first', 'second', 'third']
+
+function control(slide, dot) {
+	for (let i = 0; i < slides.length; i++) {
+		slidesArea.classList.remove(slides[i])
+	}
+	slidesArea.classList.add(slide);
+	for (let i = 0; i < dots.length; i++) {
+		dots[i].classList.remove('slider__dot_active');
+	}
+	dots[dot].classList.add('slider__dot_active');
+}
+	
+for (let i = 0; i < slides.length; i++ ) {
+	dots[i].onclick = function() {
+		control(slides[i], i);
+	}
+}
