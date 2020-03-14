24c1d05e3ccc1d9375bd1ddda6a3c35bdf51a2b5
NikAndreev <57640150+NikAndreev@users.noreply.github.com>
2020-03-14 12:47:53 +0300

    Update flowing-scroll.js

--- js/flowing-scroll.js
+++ js/flowing-scroll.js
@@ -1,10 +1,7 @@
-$(document).ready(function(){
-	    $("a[href*=#]").on("click", function(e){
-	        var anchor = $(this);
-	        $('html, body').stop().animate({
-	            scrollTop: $(anchor.attr('href')).offset().top
-	        }, 1000);
-	        e.preventDefault();
-	        return false;
-	    });
+$('a[href*="#"]').on('click', function (e) {
+  e.preventDefault();
+ 
+  $('html, body').animate({
+    scrollTop: $($(this).attr('href')).offset().top
+  }, 800, 'linear');
 });
